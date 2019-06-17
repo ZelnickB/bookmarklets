e8d4e309b3dec9a4834d2fea3cb6a443f274a806
Ben Zelnick <bnzelnick@gmail.com>
2019-06-17 18:40:00 -0400

    Updates to main and inBrowser.js file (see description)
    * Moved all function definitions into initial bookmarklets object declaration
    * Changed CSV functions to using URL Arrays to improve flexibility
    * Fixed mistakes in inBrowser.js file (invalid variable declarations, etc.)

--- Scripts/bookmarklets.js
+++ Scripts/bookmarklets.js
@@ -1,74 +1,82 @@
 var bookmarklets = {
     workspaces: {
-        // Functions are declared separately
-        compileJavaScriptFromCSV: {
-            // Functions are declared separately
-        }
-    }
-};
-bookmarklets.workspaces.appendBookmarkletComponents = function (javaScriptCode, configureJSURL, configureInitialAlert) {
-    let stringToBeAppended = "";
-    if (javaScriptCode === undefined || configureJSURL === undefined || configureInitialAlert === undefined) {
-        throw "Missing parameter(s) in bookmarklets.workspaces.appendBookmarkletComponents()";
-    }
-    if (typeof javaScriptCode !== "string" || typeof configureJSURL !== "boolean" || typeof configureInitialAlert !== "string") {
-        throw "Parameter of incorrect type in bookmarklets.workspaces.appendBookmarkletComponents()";
-    }
-    if (configureJSURL) {
-        stringToBeAppended = stringToBeAppended.concat("javascript:");
-    }
-    if (configureInitialAlert !== undefined && configureInitialAlert.split(",").length === 2) {
-        if (configureInitialAlert.split(",")[0] === "true") {
-            stringToBeAppended = stringToBeAppended.concat("if(window.confirm('Supernova Workspace Launcher: Are you sure that you would like to launch the workspace titled \"" + configureInitialAlert.split(",")[1] + "\"? Ensure that the webpage on which you are currently has permission to open popups.')){");
-        }
-    }
-    if (configureInitialAlert.split(",")[0] === "true") {
-        return stringToBeAppended + javaScriptCode + "}";
-    }
-        else {
-            return stringToBeAppended + javaScriptCode;
-        }
-};
-bookmarklets.workspaces.compileJavaScriptFromCSV.assemble = function (CSV) {
-    if (CSV === undefined) {
-        throw "Missing parameter(s) in bookmarklets.workspaces.compileJavaScriptFromCSV.assemble()";
-    }
-    if (typeof CSV !== "string") {
-        throw "Parameter of incorrect type in bookmarklets.workspaces.compileJavaScriptFromCSV.assemble()";
-    }
-    let URLArray = CSV.split(",");
-    let iterations = 0;
-    let compiledCode = "";
-    while (iterations < URLArray.length) {
-        compiledCode = compiledCode + "window.open('" + URLArray[iterations] + "'); ";
-        iterations++;
-    }
-    return compiledCode;
-};
-bookmarklets.workspaces.createCompleteBookmarkletURLFromCSV = function (CSV, workspaceTitle) {
-    if (CSV === undefined) {
-        throw "Missing parameter(s) in bookmarklets.workspaces.createCompleteBookmarkletURLFromCSV()";
-    }
-    if (typeof CSV !== "string") {
-        throw "Parameter of incorrect type in bookmarklets.workspaces.createCompleteBookmarkletURLFromCSV()";
-    }
-    let iterations = 0;
-    let URLArray = CSV.split(",");
-    let compiledURL = "";
-    if (workspaceTitle !== undefined) {
-        compiledURL = "javascript:if(window.confirm('Supernova Workspace Launcher: Are you sure that you would like to launch the workspace titled \"" + workspaceTitle + "\"? Ensure that the webpage on which you are currently has permission to open popups.')){";
-        while (iterations < URLArray.length) {
-            compiledURL = compiledURL + "window.open('" + URLArray[iterations] + "'); ";
-            iterations++;
-        }
-        return compiledURL + "}";
-    }
-        else {
-            compiledURL = "javascript:";
-            while (iterations < URLArray.length) {
-                compiledURL = compiledURL + "window.open('" + URLArray[iterations] + "'); ";
-                iterations++;
+        appendBookmarkletComponents: function (javaScriptCode, configureJSURL, configureInitialAlert) {
+            let stringToBeAppended = "";
+            if (javaScriptCode === undefined || configureJSURL === undefined) {
+                throw String("Missing parameter(s) in bookmarklets.workspaces.appendBookmarkletComponents()");
             }
-            return compiledURL;
+            if (typeof javaScriptCode !== "string" || typeof configureJSURL !== "boolean" || (typeof configureInitialAlert !== "string" && typeof configureInitialAlert !== "undefined")) {
+                throw String("Parameter of incorrect type in bookmarklets.workspaces.appendBookmarkletComponents()");
+            }
+            if (configureJSURL) {
+                stringToBeAppended = stringToBeAppended.concat("javascript:");
+            }
+            if (configureInitialAlert !== undefined) {
+                stringToBeAppended = stringToBeAppended.concat("if(window.confirm('Are you sure that you would like to launch the workspace titled \"" + configureInitialAlert + "\"? Ensure that the webpage on which you are currently has permission to open popups. This bookmarklet was created as a Bookmarklets Workspace Launcher')){");
+            }
+            if (configureInitialAlert !== undefined) {
+                return stringToBeAppended + javaScriptCode + "}";
+            }
+                else {
+                    return stringToBeAppended + javaScriptCode;
+                }
+        },
+        compileJavaScript: {
+            assemble: function (URLArray) {
+                if (URLArray === undefined) {
+                    throw String("Missing parameter(s) in bookmarklets.workspaces.compileJavaScriptFromCSV.assemble()");
+                }
+                if (typeof URLArray !== "object") {
+                    throw String("Parameter of incorrect type in bookmarklets.workspaces.compileJavaScriptFromCSV.assemble()");
+                }
+                let iterations = 0;
+                let compiledCode = "";
+                while (iterations < URLArray.length) {
+                    compiledCode = compiledCode + "window.open('" + URLArray[iterations] + "'); ";
+                    iterations++;
+                }
+                return compiledCode;
+            }
+        },
+        createCompleteBookmarkletURL: function (URLArray, workspaceTitle) {
+            if (URLArray === undefined) {
+                throw String("Missing parameter(s) in bookmarklets.workspaces.createCompleteBookmarkletURLFromCSV()");
+            }
+            if (typeof URLArray !== "object") {
+                throw String("Parameter of incorrect type in bookmarklets.workspaces.createCompleteBookmarkletURLFromCSV()");
+            }
+            let iterations = 0;
+            let compiledURL = "";
+            if (workspaceTitle !== undefined) {
+                compiledURL = "javascript:if(window.confirm('Supernova Workspace Launcher: Are you sure that you would like to launch the workspace titled \"" + workspaceTitle + "\"? Ensure that the webpage on which you are currently has permission to open popups.')){";
+                while (iterations < URLArray.length) {
+                    compiledURL = compiledURL + "window.open('" + URLArray[iterations] + "'); ";
+                    iterations++;
+                }
+                return compiledURL + "}";
+            }
+                else {
+                    compiledURL = "javascript:";
+                    while (iterations < URLArray.length) {
+                        compiledURL = compiledURL + "window.open('" + URLArray[iterations] + "'); ";
+                        iterations++;
+                    }
+                    return compiledURL;
+                }
         }
+    },
+    tools: {
+        CSVParser: function (CSVString) {
+            if (CSVString === undefined) {
+                throw String("Missing parameter(s) in bookmarklets.tools.CSVParser()");
+            }
+            if (typeof CSVString !== "string") {
+                throw String("Parameter of incorrect type in bbookmarklets.tools.CSVParser()");
+            }
+            return CSVString.split(",");
+        },
+        appendJavaScriptURL: function (JSString) {
+            return "javascript:" + JSString;
+        }
+    }
 };
